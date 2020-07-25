a45d48b373ffef935de7a93e2c5ea64a373629ae
HuangKaiWei <huang7017@gmail.com>
2020-07-25 11:12:40 +0800

    Initial Commit

---
+++ CarControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarControl
+//
+//  Created by 黃凱偉 on 2020/7/25.
+//  Copyright © 2020 黃凱偉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
